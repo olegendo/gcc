7aef12bab6491be52d4836b83d79b6a53d54d24d
Oleg Endo <olegendo@gcc.gnu.org>
2015-07-27 23:30:53 +0900

    add ams-15.c test case.

---
+++ gcc/testsuite/gcc.target/sh/ams-15.c
@@ -0,0 +1,17 @@
+/* SImode displacement loads are as cheap as post-inc loads, so usually
+   AMS will output displacement loads if they fit.  However, in this case
+   post-inc loads should be used because it eliminates address calculation
+   insns.
+*/
+/* { dg-do compile }  */
+/* { dg-options "-O2" }  */
+
+/* { dg-final { scan-assembler-times "mov.l\t@r\[0-9]\+\\+" 3 } }  */
+/* { dg-final { scan-assembler-not "add" } }  */
+
+int*
+fun_0 (int* a, int* b)
+{
+  b[0] = a[0] ^ a[1] ^ a[2];
+  return &a[3];  
+}
