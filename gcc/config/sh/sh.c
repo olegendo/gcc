ea5edcea6c1cda3d33523244cddb4b59c365ad84
Erik Krisztián Varga <erik.varga256@gmail.com>
2015-08-18 20:38:03 +0200

    Add missing switch case to the delegate's addr_reg_mod_cost.

--- gcc/config/sh/sh.c
+++ gcc/config/sh/sh.c
@@ -13800,7 +13800,7 @@ mem_access_alternatives (sh_ams::access::alternative_set& alt,
   // If there is no FPU GP regs will be used for storing FP modes, so we
   // allow normal QIHISImode alternatives also for FP modes.
   if (!TARGET_FPU_ANY
-      || (GET_MODE_CLASS (acc_mode) != MODE_FLOAT
+      || (GET_MODE_CLASS (acc_mode) != MODE_FLOAT
 	  && GET_MODE_CLASS (acc_mode) != MODE_COMPLEX_FLOAT
 	  && GET_MODE_CLASS (acc_mode) != MODE_VECTOR_FLOAT))
     {
@@ -13961,6 +13961,8 @@ addr_reg_mod_cost (const_rtx reg, const_rtx val,
 
   switch (code)
     {
+    case REG:
+      return 0;
     case PLUS:
       if (CONST_INT_P (XEXP (val, 1)))
         return ams_reg_disp_cost (reg, INTVAL (XEXP (val, 1)), as, acc);
